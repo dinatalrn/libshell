39c64909291254c7e8b596bed911fd63fcd87e9e
Diego Santos <dinatalrn@gmail.com>
2017-05-16 22:29:06 -0300

    Ajust self-update...

--- self-update.sh
+++ self-update.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 SCRIPTNAME="$0"
-SCRIPT=$(readlink -n "$SCRIPTNAME")
+# SCRIPT=$(readlink -n "$SCRIPTNAME")
+# SCRIPTPATH=$(dirname "$SCRIPT" && pwd)
 
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTPATH=$(dirname "$SCRIPTNAME" && pwd)
+
 ARGS="$@"
 BRANCH="master"
 
 self_update_git() {
-    
-    cd $SCRIPTPATH
-    git fetch
+
+    cd $SCRIPTPATH && git fetch;
 
     # [ $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME | wc -l | grep -Eo "\d+") -ne 0 ] && {
-    [ $(git diff --name-only origin/$BRANCH | wc -l | grep -Eo "\d+") -ne 0 ] && {
+    [ $(cd $SCRIPTPATH && git diff --name-only origin/$BRANCH | wc -l | grep -Eo "\d+") -ne 0 ] && {
         
         echo "Found a new version of me, updating myself..."
         
-        git pull --force
-        git checkout -f $BRANCH
-        git pull --force
+        cd $SCRIPTPATH && git pull --force
+        cd $SCRIPTPATH && git checkout -f $BRANCH
+        cd $SCRIPTPATH && git pull --force
 
         echo "Running the new version..."
         exec "$SCRIPTNAME" "$ARGS"
