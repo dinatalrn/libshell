abdf49177a91b3753d678e0e229426eb5ff9f28a
Diego Santos <diego@MacBook-de-Diego.local>
2017-06-08 22:14:03 -0300

    upgrade

--- self-update.sh
+++ self-update.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-SCRIPTNAME="$0"
-# SCRIPT=$(readlink -n "$SCRIPTNAME")
-# SCRIPTPATH=$(dirname "$SCRIPT" && pwd)
-
-SCRIPTPATH=$(dirname "$SCRIPTNAME" && pwd)
+SCRIPT="$0"
+SCRIPTPATH=$(dirname "$SCRIPT" && pwd)
 
 ARGS="$@"
 BRANCH="master"
@@ -13,20 +10,47 @@ self_update_git() {
 
     cd $SCRIPTPATH && git fetch;
 
-    # [ $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME | wc -l | grep -Eo "\d+") -ne 0 ] && {
+    # [ $(git diff --name-only origin/$BRANCH | grep $SCRIPT | wc -l | grep -Eo "\d+") -ne 0 ] && {
     [ $(cd $SCRIPTPATH && git diff --name-only origin/$BRANCH | wc -l | grep -Eo "\d+") -ne 0 ] && {
         
-        echo "Found a new version of me, updating myself..."
-        
-        cd $SCRIPTPATH && git pull --force
-        cd $SCRIPTPATH && git checkout -f $BRANCH
-        cd $SCRIPTPATH && git pull --force
+        if [[ $# -eq 0 ]]; then
+            
+            echo "Found a new version of me, updating myself...";
 
-        echo "Running the new version..."
-        exec "$SCRIPTNAME" "$ARGS"
+            cd $SCRIPTPATH && git pull --force
+            cd $SCRIPTPATH && git checkout -f $BRANCH
+            cd $SCRIPTPATH && git pull --force
+
+            echo "Running the new version...";
+            exec "$SCRIPT" "$@ --drss-force-merge";
+
+        elif [[ ($# -eq 1) && ("$1" = "--drss-force-merge") ]]; then
+
+            echo ">>> MERGE <<<";
+
+            if [[ $(cd $SCRIPTPATH && git status | grep -o "nothing to commit") != "" ]]; then
+
+                echo "Adding changes..."
+                cd $SCRIPTPATH && git add .;
+                cd $SCRIPTPATH && git commit -m "merge";
+
+            fi
+
+            echo "Running update..."
+            cd $SCRIPTPATH && git pull && git push;
+
+            echo "Running the new version after merge...";
+            exec "$SCRIPT" "$@";
+
+        else
+
+            echo ">>> Problem!! <<<";
+
+        fi
 
         # Now exit this old instance
-        exit 1
+        exit 1;
+
     }
 
     echo "Already the latest version."
@@ -50,9 +74,13 @@ main() {
    echo "Running..."
 }
 
+self_update(){
+    self_update_$@
+}
+
 case "$1" in 
     git|http|test) 
-        self_update_$ARGS
+        self_update $ARGS
         main
     ;;
     * )
